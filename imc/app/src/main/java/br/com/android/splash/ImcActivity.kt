324c909e61eeb0d118796d038b1661ac7fd07e39
Victor klippel <victor.klippel@quaestumbrasil.com>
2020-03-25 09:55:05 -0300

    first commit

---
+++ imc/app/src/main/java/br/com/android/splash/ImcActivity.kt
@@ -0,0 +1,12 @@
+package br.com.android.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
